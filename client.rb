e0d1c08a629f51e823f307d05cbb8a24327ab828
Palash Nigam <npalash25@gmail.com>
2018-11-23 14:10:05 +0530

    CLI: Typecast port and delay to integer

--- client.rb
+++ client.rb
@@ -2,8 +2,6 @@ require 'optparse'
 require 'optparse/uri'
 require 'socket'
 
-
-
 options = {
 	:ip => 'seashells.io',
 	:port => '1337'
@@ -14,10 +12,10 @@ OptionParser.new do |opts|
 	opts.on("-i", "--ip [IP | URI]", "custom IP address or URI; default: seashells.io") do |ip|
 		options[:ip] = ip
 	end
-	opts.on("-p", "--port [Port]", "port for the IP; default: 1337") do |port|
+	opts.on("-p", "--port [Port]", Integer, "port for the IP; default: 1337") do |port|
 		options[:port] = port
 	end
-	opts.on("-d", "--delay [Delay Time]", "Time delay for piping output in seconds") do |delay|
+	opts.on("-d", "--delay [Delay Time]", Integer, "Time delay for piping output in seconds") do |delay|
 		options[:delay] = delay
 	end
 	opts.on("-o", "--output", "output to both console and server or just server") do |output|
